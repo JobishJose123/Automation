0d13df02385df5e503a26f66c8b676320d234968
syam.krishna <syam.krishna@FLY-DT189.flytxt.com>
2018-06-01 11:56:14 +0530

    new latest

--- src/test/java/stepDefinitions/IntentManagement.java
+++ src/test/java/stepDefinitions/IntentManagement.java
@@ -148,6 +148,10 @@ public class IntentManagement extends Init{
 		touchpointPage.navigateToApi();
 	}
 
+	@Then("^navigate to smart phone app$")
+	public void navigate_to_smartphone() throws Throwable {
+		touchpointPage.navigateTosmartphone();
+	}
 	@Then("^create api touchpoint from sheet \"([^\"]*)\"$")
 	public void create_api_touchpoint(String sheet) throws Throwable {
 		eh.setExcelFile("touchpointInputData", sheet);
@@ -183,6 +187,74 @@ public class IntentManagement extends Init{
 	  touchpointPage.createUssdTouchpoint(name);
 	}
 
+	
+	@Then("^create new touch point without filling mandatory fields$")
+	public void createUssdTouchpointWithoutMandatoryFields() throws Throwable {
+	  touchpointPage.createUssdTouchpointWithoutMandatoryFields();
+	}
+	
+	@Then("^create new API touch point without filling mandatory fields$")
+	public void createAPITouchpointWithoutMandatoryFields() throws Throwable {
+	  touchpointPage.createAPITouchpointWithoutMandatoryFields();
+	}
+	
+	@Then("^create new trigger touch point without filling mandatory fields$")
+	public void createTriggerTouchpointWithoutMandatoryFields() throws Throwable {
+	  touchpointPage.createTriggerTouchpointWithoutMandatoryFields();
+	}
+	@Then("^create new SMS touch point without filling mandatory fields$")
+	public void createSMSTouchpointWithoutMandatoryFields() throws Throwable {
+	  touchpointPage.createSMSTouchpointWithoutMandatoryFields();
+	}
+	
+	@Then("^create new customer care touch point without filling mandatory fields$")
+	public void createCustomerCareTouchpointWithoutMandatoryFields() throws Throwable {
+	  touchpointPage.createCustomerCareTouchpointWithoutMandatoryFields();
+	}
+	@Then("^create new smart phone app touch point without filling mandatory fields$")
+	public void createSmartPhoneAppTouchpointWithoutMandatoryFields() throws Throwable {
+	  touchpointPage.createCustomerSmartPhoneAppWithoutMandatoryFields();
+	}
+	
+	@Then("^verify validation error messages for USSD touch point$")
+	public void verifyValidationErrorMessagesInUSSDCreation() throws Throwable {
+		
+		touchpointPage.verifyValidationErrorMessagesInUSSDCreation();
+		
+	}
+	
+	@Then("^verify validation error messages for API touch point$")
+	public void verifyValidationErrorMessagesInAPICreation() throws Throwable {
+		
+		touchpointPage.verifyValidationErrorMessagesInAPICreation();
+		
+	}
+	
+	@Then("^verify validation error messages for trigger touch point$")
+	public void verifyValidationErrorMessagesInTriggerCreation() throws Throwable {
+		
+		touchpointPage.verifyValidationErrorMessagesInTriggerCreation();
+		
+	}
+	
+	@Then("^verify validation error messages for SMS touch point$")
+	public void verifyValidationErrorMessagesInSMSCreation() throws Throwable {
+		
+		touchpointPage.verifyValidationErrorMessagesInSMSCreation();
+		
+	}
+	@Then("^verify validation error messages for customer care touch point$")
+	public void verifyValidationErrorMessagesInCustomercareCreation() throws Throwable {
+		
+		touchpointPage.verifyValidationErrorMessagesInCustomercareCreation();
+		
+	}
+	@Then("^verify validation error messages for smart phone app touch point$")
+	public void verifyValidationErrorMessagesInSmartPhoneApp() throws Throwable {
+		
+		touchpointPage.verifyValidationErrorMessagesInSmartPhoneApp();
+		
+	}
 	@Then("^check ussd touchpoint in grid \"([^\"]*)\"$")
 	public void check_ussd_touchpoint_in_grid(String sheet) throws Throwable {
 		eh.setExcelFile("touchpointInputData", sheet);
@@ -1212,6 +1284,13 @@ System.out.println(editname+"program has edited successfully");
 			
 		}
 		
+				@Then("^edit customer care touchpoint with security alert$")
+		public void editCustomerCareTouchpointWithSecurityAlert() throws Throwable {
+			
+			touchpointPage.editCustomerCareTouchpointWithSecurityAlert();
+			
+			
+		}
 		@Then("^create new touchpoint with alert value$")
 		public void createTouchpointWithAlertValue() throws Throwable {
 			
@@ -1347,5 +1426,6 @@ System.out.println(editname+"program has edited successfully");
 		  
 
 		}
+		
 	
 }
