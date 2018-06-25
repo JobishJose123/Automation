ef77b9c5d546fe925f95149dbdf763443f5b01f7
joel.jose <joel.jose@FLY-LP457.flytxt.com>
2018-06-25 14:21:53 +0530

    m

--- src/test/java/baseClasses/RandonNumberGenerator.java
+++ src/test/java/baseClasses/RandonNumberGenerator.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class RandonNumberGenerator {
 	
-	public static int getRandonNumber() {
+	public static int getRandomNumber() {
 		Random rn = new Random();
  		int  n = rn.nextInt(5000) + 1;
  		return n;
