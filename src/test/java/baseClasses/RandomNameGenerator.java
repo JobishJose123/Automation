63b4ad54404d1476ff427fef39e2131f3ae7325e
joel.jose <joel.jose@FLY-LP457.flytxt.com>
2018-06-26 16:42:01 +0530

    RandomNameGenerator: removing edit and duplicate keywords

--- src/test/java/baseClasses/RandomNameGenerator.java
+++ src/test/java/baseClasses/RandomNameGenerator.java
@@ -7,7 +7,13 @@ public class RandomNameGenerator {
 		public static String getRandomName(String name) {
 			Random rn = new Random();
 	 		int  n = rn.nextInt(5000) + 1;
-	 		name =  name.replaceAll("[0-9]", "")+n;
+	 		name =  name.replaceAll("[-_][Ee]dited", "");
+	 		name =  name.replaceAll("[-_]*[Ee]dit", "");
+	 		name =  name.replaceAll("[-_]*[Dd]upe", "");
+	 		name =  name.replaceAll("[-_]*[Dd]uplicate", "");
+	 		name =  name.replaceAll("[0-9]", "");
+	 		name = name.trim();
+	 		name =  name+n;
 	 		return name;
 		}
 }
