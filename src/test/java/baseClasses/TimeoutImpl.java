40d595d676963f9d994de946acb3c393df52d1dc
joel.jose <joel.jose@FLY-LP747.flytxt.com>
2018-08-14 15:45:59 +0530

    timeout class

---
+++ src/test/java/baseClasses/TimeoutImpl.java
@@ -0,0 +1,26 @@
+package baseClasses;
+
+public class TimeoutImpl {
+
+	private long startTime;
+	private long currentTime;
+	public void startTimer() {
+		startTime = System.currentTimeMillis();
+	}
+	public boolean checkTimerMin(double min) {
+		currentTime = System.currentTimeMillis();
+		min = min*60*1000;
+		if((currentTime-startTime)>=min)
+			return false;
+		else
+		    return true;
+	}
+	public static void main(String[] args) throws InterruptedException {
+		TimeoutImpl t = new TimeoutImpl();
+		t.startTimer();
+		while(t.checkTimerMin(0.1)){
+			
+		}
+	}
+
+}
